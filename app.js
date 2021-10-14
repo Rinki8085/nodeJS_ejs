7da5541464beafc194278b45e457d133c721f46b
Rinki8085 <krinki2604@gmail.com>
2021-10-14 13:46:15 +0530

    dashboard

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ const app = express();
 const port = process.env.PORT || 5600;
 const mongo = require('mongodb');
 const MongoClient = mongo.MongoClient;
-const bodyParser = require('body-Parser');
-const mongourl = "mongodb://localhost:27017"
+const bodyParser = require('body-parser');
+const mongoUrl = "mongodb://localhost:27017"
 let db;
 
 //middleware
@@ -12,55 +12,66 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 
 //html file path
-app.set('views','./src/views')
-app.set('view engine','ejs')
+app.set('views', './src/views')
+app.set('view engine', 'ejs');
 
-// add new restaurent
-app.get('/newRest',(req,res)=>{
+// Add new rest
+app.get('/newrest',(req,res) => {
     res.render('admin')
 })
 
-app.post('/addRest',(req,res)=>{
+//list rest
+app.get('/',(req,res )=> {
+    db.collection('testrest').find({}).toArray((err,result) => {
+        if(err) throw err;
+        res.render('index',{data:result})
+    })
+})
+
+// Add restaurants
+app.post('/addRest',(req,res) => {
     var mealinput = req.body.mealtype
     var mealtypeArr = []
-    mealinput.map((data)=>{
-        var val = data.split(",")
+    mealinput.map((data) => {
+        var val = data.split(',')
         var obj = {}
         obj.mealtype = val[0];
         obj.name = val[1]
         mealtypeArr.push(obj)
     })
-
-    var cuisineinput = req.body.cuisine
-    var cuisinetypeArr = []
-    cuisineinput.map((data)=>{
-        var val = data.split(",")
+    var cuisineInput = req.body.cuisine
+    var cuisineArr = []
+    cuisineInput.map((data) => {
+        var val = data.split(',')
         var obj = {}
         obj.cuisine = val[0];
         obj.name = val[1]
-        cuisinetypeArr.push(obj)
+        cuisineArr.push(obj)
     })
-
     var body = {
-        "_id":Math.floor(Math.random()*100000),
-        "city_name":(req.body.city.split(','))[1],
-        "city":(req.body.city.split(','))[0],
+        "_id":Math.floor(Math.random()*10000),
+        "city_name":req.body.city.split(',')[1],
+        "city":req.body.city.split(',')[0],
         "name":req.body.name,
         "locality": req.body.locality,
-        "thumb":req.body.thumb,
+        "thumb": req.body.thumb,
         "cost": req.body.cost,
         "address": req.body.address,
         "type": mealtypeArr,
-        "Cuisine":cuisinetypeArr
+        "Cuisine":cuisineArr
     }
-    res.send(body)
+    db.collection('testrest').insert(body,(err,result) => {
+        //res.send('data added')
+        res.redirect('/')
+    })
+    
 })
 
 //db connection
-MongoClient.connect(mongourl,(err,client) => {
-    if(err) console.log('Error while connecting')
-    db = client.db('FSWDinternship');
-    app.listen(port,(err)=>{
-        console.log(`Server is running ${port}`)
+MongoClient.connect(mongoUrl,(err,client) =>{
+    if(err) console.log(`Error while connecting`)
+    db = client.db('edurekainternship');
+    app.listen(port, (err) =>{
+        console.log(`Server is running on port ${port}`)
     })
 })
